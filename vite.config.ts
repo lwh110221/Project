4892174267147d9185c57d620828acdb21244675
lwh110221 <2285813721@qq.com>
2024-11-28 22:25:54 +0800

    修复工作流

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: process.env.VITE_BASE_URL || '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
